e737d8581351476fe6d571e4454888cbc9f718fe
Igor-Manko <manko161igor@gmail.com>
2022-06-27 22:54:10 +0300

    поиск чисел в интервале от -N до N

--- Seminar_1/Example_003/Program.cs
+++ Seminar_1/Example_003/Program.cs
@@ -1,1 +1,12 @@
 ﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int negativ_num = num * -1;
+
+while (negativ_num <= num)
+{
+    Console.WriteLine(negativ_num);
+    negativ_num ++;
+}
