11aee47a2e71cc72341f9810c7028022c70db878
Igor-Manko <manko161igor@gmail.com>
2022-06-27 22:10:47 +0300

    добавил пример Example 002  с выбором дня недели

---
+++ Seminar_1/Example_002/Program.cs
@@ -0,0 +1,71 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+int num = 0;
+Console.WriteLine("Введите число от 1 до 7: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+switch (num)
+{
+    case 1:{
+        Console.WriteLine("Понедельник");
+        break;
+    }
+    case 2:{
+        Console.WriteLine("Вторник");
+        break;
+    }
+    case 3:{
+        Console.WriteLine("Среда");
+        break;
+    }
+    case 4:{
+        Console.WriteLine("Четверг");
+        break;
+    }
+    case 5:{
+        Console.WriteLine("Пятница");
+        break;
+    }
+    case 6:{
+        Console.WriteLine("Суббота");
+        break;
+    }
+    case 7:{
+        Console.WriteLine("Воскресенье");
+        break;
+    }
+}
+
+/*
+Console.WriteLine("Введите номер дня недели от 1 до 7: ");
+string number = Console.ReadLine();
+
+if (number == "1")
+{
+    Console.WriteLine("Понедельник");
+}
+if (number == "2")
+{
+    Console.WriteLine("Вторник");
+}
+if (number == "3")
+{
+    Console.WriteLine("Среда");
+}
+if (number == "4")
+{
+    Console.WriteLine("Четверг");
+}
+if (number == "5")
+{
+    Console.WriteLine("Пятница");
+}
+if (number == "6")
+{
+    Console.WriteLine("Суббота");
+}
+if (number == "7")
+{
+    Console.WriteLine("Воскресенье");
+}
+*/
