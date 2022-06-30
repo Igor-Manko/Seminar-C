7a65497fa6884f8f9ca9a6ad6d0dfe9636b318f9
Igor-Manko <manko161igor@gmail.com>
2022-07-01 00:53:05 +0300

    add Example 004 кратность 7 и 23

---
+++ Seminar_2/Example_004/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+string showNumber()
+{
+    Console.WriteLine("Введите число:");
+    int num = Convert.ToInt32(Console.ReadLine());
+    //Console.WriteLine("Введите второе число:");
+    //int num2 = Convert.ToInt32(Console.ReadLine());
+
+    if (num % 7 == 0 & num % 23 == 0)
+    {
+        return "Введенное число кратно 7 и 23 одновременно";
+    }
+    else
+    {
+        return "Введенное число НЕ кратно 7 и 23 одновременно";
+    }
+}
+
+Console.WriteLine(showNumber());
