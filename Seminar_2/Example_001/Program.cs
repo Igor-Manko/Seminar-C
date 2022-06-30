e2aebda3e9f39f948a1e0e89545d9009d60b446c
Igor-Manko <manko161igor@gmail.com>
2022-06-30 23:20:01 +0300

    Example 001 результат удаления цифры в 3значном числе

---
+++ Seminar_2/Example_001/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите функцию, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int num = new Random().Next(100, 1000);  // рандомное трехзначное число
+Console.WriteLine($"Сгенерировано случайное число : {num}");
+
+int showNumber(int num1)
+{
+    int firstNum = num1 / 100;
+    int secondNum = num1 % 10;
+    int newNum = firstNum * 10 + secondNum;
+
+    Console.WriteLine($"Первая цифра: {firstNum}");
+    Console.WriteLine($"Третья цифра: {secondNum}");
+
+    return newNum;
+}
+
+Console.WriteLine($"Новое число без второй цифры: {showNumber(num)}");
