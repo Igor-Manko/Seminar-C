48abd85180263b6819f99b101bdfa6e0825b588b
Igor-Manko <manko161igor@gmail.com>
2022-07-02 00:36:20 +0300

    в результате квадраты чисел до N

--- Seminar_3/Example_004/Program.cs
+++ Seminar_3/Example_004/Program.cs
@@ -1,31 +1,36 @@
 ﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу 
 //квадратов чисел от 1 до N.
-/*
+
+// Вариант 1
+
+Console.Write("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
 int Sqrt2(int num)
 {
     int a = num * num;
     return a;
 }
 
-Console.Write("Введите число : ");
-int number = Convert.ToInt32(Console.ReadLine());
-
 int index = 1;
-while (index <= number)
-{
-    Console.WriteLine(Sqrt2(index));
+while (index <= number) {
+    Console.Write(Sqrt2(index) + " ");
     index++;
-}*/
+}
+/*
+
+// Вариант 2
+
 Console.Write("Введите число : ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int powTable(int n)
+int squareTable(int n)
 {
     for (int i = 1; i <= n; i++)
     {
-        Console.Write(i * i + "|");
+        Console.Write(i * i + " | ");
     }
     Console.WriteLine();
     return n;
 }
-powTable(number);
+squareTable(number); */
