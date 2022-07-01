0d145bf7e7fc088a5ecc5d31e215ee98705af258
Igor-Manko <manko161igor@gmail.com>
2022-07-02 00:25:15 +0300

    Расчет расстояния между координатами в 2D

--- Seminar_3/Example_003/Program.cs
+++ Seminar_3/Example_003/Program.cs
@@ -1,44 +1,21 @@
 ﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
 
-Console.WriteLine("Введите координаты первой точки X & Y:");
+Console.WriteLine("Введите координаты первой точки");
+Console.Write("X1 :");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y1 :");
+int b = Convert.ToInt32(Console.ReadLine());
 
-int x1 = Convert.ToInt32(Console.ReadLine());
-int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки");
+Console.Write("X2 :");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y2 :");
+int d = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координаты второй точки X & Y:");
-int x2 = Convert.ToInt32(Console.ReadLine());
-int y2 = Convert.ToInt32(Console.ReadLine());
-
-
-
-double newPoint()
+double newPoint(int x1, int y1, int x2, int y2)
 {
-    double newPoint = Math.Sqrt(Math.Pow(x2-x1, 2)+ Math.Pow(y2-y1, 2));
-    return newPoint;
-}
-
-
-Console.WriteLine(newPoint()); 
-/*
-Console.WriteLine ("Введите координаты первой точки");
-Console.Write ("X1 :");
-int n1 = Convert.ToInt32(Console.ReadLine());
-Console.Write ("Y1 :");
-int n2 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine ("Введите координаты второй точки");
-Console.Write ("X2 :");
-int n3 = Convert.ToInt32(Console.ReadLine());
-Console.Write ("Y2 :");
-int n4 = Convert.ToInt32(Console.ReadLine());
-
-double Distance (int x1,  int y1, int x2, int y2)
-{
-    double dis = Math.Sqrt((x2-x1)*(x2-x1)+(y2 -y1)*(y2 -y1));
+    double dis = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
     return dis;
-
 }
 
-
-
-Console.WriteLine(Distance(n1,n2,n3,n4));*/
+Console.WriteLine(newPoint(a, b, c, d)); 
