b58b02395683a532a32415c7d32770076c093fdf
Igor-Manko <manko161igor@gmail.com>
2022-07-01 23:47:27 +0300

    выбор плоскости на координатной оси

--- Seminar_3/Example_002/Program.cs
+++ Seminar_3/Example_002/Program.cs
@@ -1,92 +1,43 @@
 ﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
 
-/*
-string findQvart(int Quote)
-{
-    if (Quote == 1){
-        return "Все положительные";
-    }
-     if (Quote == 2){
-        return "По X отрицательные, по Y положительные";
-    }
-     if (Quote == 3){
-        return "Все отрицательные";
-    }
-     if (Quote == 4){
-        return "X положительный, Y отрицательный";
-    }
-    return "err";
-}
-*/
+//     .............................
+//     .            .              .
+//     .            .              .
+//     .     2      .       1      .
+//     .            .              .
+//     .............................
+//     .            .              .
+//     .            .              .
+//     .     3      .       4      .
+//     .            .              .
+//     .............................
 
-Console.WriteLine("Введите номер четверти: ");
+
+Console.WriteLine("Введите номер четверти на координатной оси: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-string Quote(int num)
+string Quote(int a)
 {
-    if (num >= 1 && num <= 4)
+    switch (a)
     {
-        switch (num)
-        {
-            case 1:
-                {
-                    return "Все положительные";
-                    
-                }
-            case 2:
-                {
-                    Console.WriteLine("По X отрицательные, по Y положительные");
-                    break;
-                }
-            case 3:
-                {
-                    Console.WriteLine("Все отрицательные");
-                    break;
-                }
-            case 4:
-                {
-                    Console.WriteLine("X положительный, Y отрицательный");
-                    break;
-                }
-        }
-
+        case 1:
+            {
+                return "X > 0, Y > 0";
+            }
+        case 2:
+            {
+                return "X < 0, Y > 0";
+            }
+        case 3:
+            {
+                return "X < 0, Y < 0";
+            }
+        case 4:
+            {
+                return "X > 0, Y < 0";
+            }
     }
     return "err";
 }
 
 Console.WriteLine(Quote(number));
-
-/*
-string ShowCoord ( int number)
-{
-    switch (number)
-    {
-        case  1: 
-        {
-            return " x > 0 и у >0";
-            
-        }
-        case 2: 
-         {   
-            return " x < 0 и у >0";
-            
-         }
-        case  3: 
-         {   
-            return " x < 0 и у <0";
-            
-         }
-        case  4: 
-        {
-            return " x > 0 и у < 0";
-            
-        }
-    }
-    return "ошибка";
-}
-
-Console.Write ("Введите номер четверти : ");
-int num = Convert.ToInt32(Console.ReadLine());
-
-Console.Write(ShowCoord(num));
-*/
