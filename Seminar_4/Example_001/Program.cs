201082be1e45908acbc374ab4170629c04aa2c77
Igor-Manko <manko161igor@gmail.com>
2022-07-05 22:02:13 +0300

    sum from 1 before N

---
+++ Seminar_4/Example_001/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+Console.Write("Введите число :");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+int mySum(int numbers)
+{
+    int sum = 0;
+    for (int i = 1; i <= numbers; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+Console.WriteLine(mySum(num));
