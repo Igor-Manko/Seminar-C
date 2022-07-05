a81726cc11d478cc407d4ee491ba9af8d8e6576a
Igor-Manko <manko161igor@gmail.com>
2022-07-05 23:02:13 +0300

    factorial from 1 to N

---
+++ Seminar_4/Example_003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+int myFactorial(int num)
+{
+    int fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact = fact * i;
+    }
+    return fact;
+}
+
+Console.Write("Введите число для расчета факториала: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.WriteLine("Значение факториала числа: " + myFactorial(numbers));
