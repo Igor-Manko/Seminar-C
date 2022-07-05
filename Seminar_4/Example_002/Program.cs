e69530b1e7d5a754d06504208d64dd790258ee90
Igor-Manko <manko161igor@gmail.com>
2022-07-05 22:32:01 +0300

    number of digits in a number

---
+++ Seminar_4/Example_002/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+int myCounter(int numbers)
+{
+    int i = 0;
+    while (numbers > 0)
+    {
+        numbers = numbers / 10;
+        i++;
+    }
+    return i;
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Количество цифр в числе: " + myCounter(num));
