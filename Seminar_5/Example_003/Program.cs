f7df5731451f6daada384ac568e7fe16b96c2e8d
Igor-Manko <manko161igor@gmail.com>
2022-07-20 16:23:29 +0700

    find numbers in array

---
+++ Seminar_5/Example_003/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+
+
+int[] fillArray(int[] arr)
+{
+   
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 10);
+    }
+    return arr;
+}
+
+string findArray(int[] arr, int num)
+{
+   // int find = 3;
+    //int count;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == num) return ($"Число {num} есть в массиве");
+        //else Console.WriteLine($"Чмсло {find} отсутствует в массиве");
+    }
+    return "отсутствует";
+
+}
+
+ int[] arr = new int[5];
+
+Console.WriteLine(String.Join(" ", fillArray(arr)));
+Console.WriteLine(String.Join(" ", findArray(arr, 3)));
